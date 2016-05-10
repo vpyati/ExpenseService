a70dfc216a519d0adee45830a354528906aef5e3
vipyati <vipyati@ebay.com>
2016-05-10 09:28:05 +0530

    Sort in descending order

--- src/main/java/com/vikram/db/awsdynamodb/AwsExpenseStore.java
+++ src/main/java/com/vikram/db/awsdynamodb/AwsExpenseStore.java
@@ -23,16 +23,6 @@ public class AwsExpenseStore extends AbstractAwsDb implements ExpenseStore {
 	public void add(ExpenseDo expense) {	
 		
 		try {
-//			PutItemResult result = amazonDynamoDb.putItem(new PutItemRequest().withTableName("Expense_Modified").addItemEntry("Name", new AttributeValue(expense.getName()))
-//					.addItemEntry("Description", new AttributeValue(expense.getDescription()))
-//					.addItemEntry("CreationDate", new AttributeValue().withN(String.valueOf(expense.getAmazonRangeKey())))
-//					.addItemEntry("UID", new AttributeValue(expense.getuID()))
-//					.addItemEntry("Tags", new AttributeValue(expense.getTags()))
-//					.addItemEntry("Category", new AttributeValue(expense.getCategory()))
-//					.addItemEntry("Currency", new AttributeValue("INR"))
-//					.addItemEntry("Amount", new AttributeValue(expense.getAmount()))
-//					);
-			
 			DynamoDBMapper mapper = new DynamoDBMapper(amazonDynamoDb);
 			mapper.save(expense);
 			
@@ -58,7 +48,8 @@ public class AwsExpenseStore extends AbstractAwsDb implements ExpenseStore {
 		 DynamoDBQueryExpression<ExpenseDo> queryExpression = new DynamoDBQueryExpression<ExpenseDo>()
 		            .withKeyConditionExpression("UID = :val1 AND CreationDate BETWEEN :val2 AND :val3")
 		            .withExpressionAttributeValues(eav)
-		            .withSelect(Select.ALL_ATTRIBUTES);
+		            .withSelect(Select.ALL_ATTRIBUTES)
+		            .withScanIndexForward(false);
 		 
 		List<ExpenseDo> expenses =  mapper.query(ExpenseDo.class, queryExpression);
 		
